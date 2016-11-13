af7daa95d055eae80425a5b2de40fd98a43629ce
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-11-12 20:29:58 -1000

    yep

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,13 +1,21 @@
 
-var displayNum = 0;
 
-function AddDigit(x) {     //ADD A DIGIT TO DISPLAY (kept as 'Current')
-       if (eval(displayNum) == 0)
-        && (displayNum.indexOf(".") == -1)
-           { displayNum = x;
-           } else
-           { displayNum = displayNum + x;
-           };
-    }; };
-   document.Calculator.Display.value = displayNum;
- }
+var calculator = calculatorModule();
+ display= 0;
+ document.getElementById("btn1").addEventListener('click', function(){
+  document.getElementById("display").innerHTML += 1;
+ });
+
+ btn2.addEventListener('click', function() {display.innerHTML += 2;});
+ btn3.addEventListener('click', function() {display.innerHTML += 3;});
+ btn4.addEventListener('click', function() {display.innerHTML += 4;});
+ btn5.addEventListener('click', function() {display.innerHTML += 5;});
+ btn6.addEventListener('click', function() {display.innerHTML += 6;});
+ btn7.addEventListener('click', function() {display.innerHTML += 7;});
+ btn8.addEventListener('click', function() {display.innerHTML += 8;});
+ btn9.addEventListener('click', function() {display.innerHTML += 9;});
+ btn0.addEventListener('click', function() {display.innerHTML += 0;});
+ btn00.addEventListener('click', function() {display.innerHTML += 00;});
+ 
+
+ add.addEventListener('click', add(Display));
